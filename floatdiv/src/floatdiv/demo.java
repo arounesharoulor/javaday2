ac34bf138bef16c8887881f6c400e188188a832b
A.Arounesh <110887451+hsenuora@users.noreply.github.com>
2024-06-27 12:12:31 +0530

    demo

---
+++ floatdiv/src/floatdiv/demo.java
@@ -0,0 +1,16 @@
+package floatdiv;
+
+public class demo {
+	public static void main(String argv[]) {
+		int value1= 9/2;
+		float value2= 101/62;
+		double value3= 10d/64;
+		float value4=5/2.5f;
+		System.out.println("value 1=" + value1);
+		System.out.println("value 2=" + value2);
+		System.out.println("value 3=" + value3);
+		System.out.println("value 4=" + value4);
+	}
+	
+
+}
