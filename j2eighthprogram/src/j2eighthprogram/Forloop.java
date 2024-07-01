c1125af61edb50bd35ba5de0ab8610c8998dadcd
A.Arounesh <110887451+hsenuora@users.noreply.github.com>
2024-07-01 11:46:08 +0530

    forloop

---
+++ j2eighthprogram/src/j2eighthprogram/Forloop.java
@@ -0,0 +1,12 @@
+package j2eighthprogram;
+
+public class Forloop {
+	public static void main(String arg[]) {
+	for(int i=1;i<=1000;i++)
+	{
+		System.out.println("value of i:");
+		System.out.println(i);
+	}
+	}
+
+}
