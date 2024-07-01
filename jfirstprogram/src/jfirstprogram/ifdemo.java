6e9fc8ce689a16c32e9a6430b3977cf8caf636d9
A.Arounesh <110887451+hsenuora@users.noreply.github.com>
2024-07-01 10:29:28 +0530

    day2 firstprg

---
+++ jfirstprogram/src/jfirstprogram/ifdemo.java
@@ -0,0 +1,11 @@
+package jfirstprogram;
+public class ifdemo {
+public static void main(String args[]) {
+int x=5;
+if(x!=5)
+	System.out.println("value of x is not 5");
+System.out.println("value of x 5");
+	
+	
+}
+}
